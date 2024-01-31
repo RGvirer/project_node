c7cce7f0e756be53e0c3e34e478397e7e00634dc
RGvirer <150089166+RGvirer@users.noreply.github.com>
2024-01-31 21:41:00 +0200

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        const mongoURI = process.env.DB_CONNECTION || 'mongodb+srv://118rgv:d7c-dBFPPf$J$Lc@cluster0.gixnso0.mongodb.net/?retryWrites=true&w=majority';
+        const mongoURI = process.env.DB_CONNECTION || "mongodb://localhost:27017/store";
         await mongoose.connect(mongoURI);
         console.log("succeded to connect mongoDB");
     }
