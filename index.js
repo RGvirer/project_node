772841bdd894dabdfa04f961c9f6d2c28aa6bf3b
RGvirer <118rgv@gmail.com>
2024-01-31 21:46:22 +0200

    secondCommit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app=express();
 app.use(express.static('images'));
 app.use(morgan("common"));
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"https://storeserver-uoax.onrender.com"}));
 app.use("/api/products",productRouter);
 app.use("/api/users",userRouter);
 app.use("/api/orders",orderRouter);
