7cfb56449ed87d0e5847b83688c76c4b81f0c024
RGvirer <118rgv@gmail.com>
2024-02-11 19:19:50 +0200

    commit11

--- models/inspirationalPhotos.js
+++ models/inspirationalPhotos.js
@@ -6,9 +6,11 @@ const inspirationalPhotosSchema = mongoose.Schema(
     {
         name: { type: String, required: true },
         description: { type: String, required: true },
-        manufacturingDate: { type: Date },
-        routingToImage: { type: String, required: true },
-        coordinatePoints: [{ type: String}],
+        routingToImage:{type:String},
+        productDetails: 
+        // אני רוצה שבתוך השדה פרטי התמונה יהיו אובייקטים עם השדות הבאים
+        //coordinatePoints-שיכיל את הקואורדינטה בה המוצר נמצא בתוך התמונה
+        //productId-ניתוב למוצר ע"י id
     }
 );
 
@@ -19,9 +21,7 @@ export const inspirationalPhotosValidator = (_inspirationalPhotosValidate) => {
     let productJoi = joi.object({
         name: joi.string().required(),
         description: joi.string().required(),
-        manufacturingDate: joi.date().default(Date.now),
         routingToImage: joi.string().required(),
-        coordinatePoints: joi.array().items(joi.string())
     });
     return productJoi.validate(_inspirationalPhotosValidate)
 }
