7332ef9df0f57535f4810e86bebe645942518db2
RGvirer <118rgv@gmail.com>
2024-02-11 04:37:10 +0200

    commit9

--- models/product.js
+++ models/product.js
@@ -21,7 +21,7 @@ export const productValidator = (_productToValidate) => {
         description:joi.string().required(),
         manufacturingDate:joi.date().default(Date.now),
         routingToImage: joi.string().default('https://storeserver-uoax.onrender.com/מיכאל.jpg').replace(/^/, 'https://storeserver-uoax.onrender.com/'),
-        coordinatePoints:joi.object().String(),
+        coordinatePoints:joi.object().string(),
     });
     return productJoi.validate(_productToValidate)
 }
