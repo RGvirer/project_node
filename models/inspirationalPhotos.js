b4c8c6f5598526b59dd909e4aacf769dbe4f6f51
RGvirer <118rgv@gmail.com>
2024-02-11 18:03:52 +0200

    commit11

--- models/inspirationalPhotos.js
+++ models/inspirationalPhotos.js
@@ -2,24 +2,26 @@ import joi from "joi"
 import mongoose from "mongoose"
 
 // mongoose.Schema() הגדרת מודל סכימה של מוצר באמצעות הפונקציה 
-const productSchema = mongoose.Schema(
+const inspirationalPhotosSchema = mongoose.Schema(
     {
         name: { type: String, required: true },
         description: { type: String, required: true },
         manufacturingDate: { type: Date },
         routingToImage: { type: String, required: true },
+        coordinatePoints: [{ type: String}],
     }
 );
 
-export const Product = mongoose.model("product", productSchema);
+export const Product = mongoose.model("product", inspirationalPhotosSchema);
 
 // Joi פונקציה המאמתת את המוצר, המתבססת על סכימה מוגדרת מראש באמצעות ספריית 
-export const productValidator = (_productToValidate) => {
+export const inspirationalPhotosValidator = (_inspirationalPhotosValidate) => {
     let productJoi = joi.object({
         name: joi.string().required(),
         description: joi.string().required(),
         manufacturingDate: joi.date().default(Date.now),
         routingToImage: joi.string().required(),
+        coordinatePoints: joi.array().items(joi.string())
     });
-    return productJoi.validate(_productToValidate)
+    return productJoi.validate(_inspirationalPhotosValidate)
 }
