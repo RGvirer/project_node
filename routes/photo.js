b9f75c105aad3abba8dc5a95c9588dfa50eb6e5d
RGvirer <118rgv@gmail.com>
2024-02-12 16:42:35 +0200

    commit1

--- routes/photo.js
+++ routes/photo.js
@@ -1,5 +1,5 @@
 import express from "express";
-import * as getAllPhotos from '../controllers/photo.js'
+import { getAllPhotos } from '../controllers/photo.js'
 
 const router = express.Router();
 router.get("/", getAllPhotos);
