66434a1bd2291e8db52524de181cdb0a8f45099c
RGvirer <118rgv@gmail.com>
2024-02-13 13:29:58 +0200

    commit5

--- models/photo.js
+++ models/photo.js
@@ -2,16 +2,20 @@ import joi from "joi"
 import mongoose from "mongoose"
 import { productSchema } from "./product.js";
 
-// mongoose.Schema() הגדרת מודל סכימה של מוצר באמצעות הפונקציה 
 const photoSchema = mongoose.Schema(
     {
         name: { type: String, required: true },
         description: { type: String, required: true },
-        routingToImage: { type: String,required:true },
-        imageDetails: {type:[{ coordinatePoints: {type:String,required:true} , product:{productSchema}}],required:true}
+        routingToImage: { type: String, required: true },
+        imageDetails: { 
+            type: [{ 
+                coordinatePoints: { type: String, required: true }, 
+                product: { type: productSchema } 
+            }], 
+            required: true 
+        }
     }
 );
-
 export const Photo = mongoose.model("photo", photoSchema);
 
 export const photoValidator = (_photoToValidate) => {
@@ -19,15 +23,19 @@ export const photoValidator = (_photoToValidate) => {
         name: joi.string().required(),
         description: joi.string().required(),
         routingToImage: joi.string().required(),
-        imageDetails: joi.array().items(joi.object({
-            coordinatePoints: joi.string().required(),
-            product: joi.object({
-                name: joi.string().required(),
-                description: joi.string().required(),
-                manufacturingDate: joi.date().default(Date.now()),
-                routingToImage: joi.string().required(),
-            }).required()
-        })).required()
+        imageDetails: joi.array().items(joi.object(
+            {
+                coordinatePoints: joi.string().required(),
+                product: joi.object(
+                    {
+                        name: joi.string().required(),
+                        description: joi.string().required(),
+                        manufacturingDate: joi.date().default(Date.now()),
+                        routingToImage: joi.string().required(),
+                    }
+                ).required()
+            }
+        )).required()
     });
     return photoJoi.validate(_photoToValidate);
 }
