90585cb30294dc400cd6d2b41a5cc62837167fcd
RGvirer <118rgv@gmail.com>
2024-03-14 02:40:04 +0200

    update users errors messeges

--- controllers/user.js
+++ controllers/user.js
@@ -8,13 +8,13 @@ export const addNewUser = async (req, res) => {
         let { name, password, email } = req.body;
         let validate = userValidator(req.body);
         if (!/[0-9]{2}[A-Za-z]{2}/.test(password))
-            return res.status(400).send("password is not valid");
+            return res.status(400).send("סיסמה לא תקינה");
         if (validate.error) {
             res.status(400).send(validate.error.details[0]);
         }
         let sameUsers = await User.find(req.body);
         if (sameUsers.length > 0) {
-            res.status(409).send("this user already exists ");
+            res.status(409).send("המשתמש הזה כבר קיים במערכת ");
         }
         let hushPassword = await bcyript.hash(password, 15);
         let newUser = new User({ name, password: hushPassword, email });
@@ -24,7 +24,7 @@ export const addNewUser = async (req, res) => {
         res.json({ _id, userName, userEmail, token });
     }
     catch (err) {
-        res.status(400).send("an error occured in: " + err.message);
+        res.status(400).send("הטעות ב: " + err.message);
     }
 }
 
@@ -34,21 +34,21 @@ export const getAllUsers = async (req, res) => {
         res.json(allUsers);
     }
     catch (err) {
-        res.status(500).send("an error occured in: " + err.message);
+        res.status(500).send("הטעות ב: " + err.message);
     }
 }
 export const getUserById = async (req, res) => {
     try {
         let { id } = req.params;
         if (!mongoose.isValidObjectId(id))
-            return res.status(400).send("this id is not valid");
+            return res.status(400).send("id לא תקין");
         let user = await User.findById(id);
         if (!user)
-            return res.status(404).send("no such product");
+            return res.status(404).send("אין כזה משתמש");
         res.json(user);
     }
     catch (err) {
-        res.status(400).send("The user cannot be received " + err.message);
+        res.status(400).send("לא אפשרי להביא את המשתמש: " + err.message);
     }
 }
 
@@ -56,20 +56,20 @@ export const login = async (req, res) => {
     try {
         let { email, password } = req.body;
         if (!email || !password)
-            return res.status(404).send("missing required parameter");
+            return res.status(404).send("חסר שדות חובה");
         let user = await User.findOne({ email });
         if (!user)
-            return res.status(404).send("there is no user with such credentials");
+            return res.status(404).send("לא קיים כזה משתמש");
         if (!/[0-9]{2}[A-Za-z]{2}/.test(password))
-            return res.status(400).send("password is not valid");
+            return res.status(400).send("הסיסמה לא תקינה");
         if (!await bcyript.compare(password, user.password))
-            return res.status(404).send("password error");
+            return res.status(404).send("שגיאת סיסמה");
         let { _id, name, role, email: userEmail } = user;
         let token = generateToken(user);
         res.json({ _id, name, role, userEmail, token });
     }
     catch (err) {
-        res.status(500).send("an error occured in: " + err.message);
+        res.status(500).send("השגיאה ב: " + err.message);
     }
 }
 
@@ -77,14 +77,14 @@ export const deleteUserById = async (req, res) => {
     let { id } = req.params;
     try {
         if (!mongoose.isValidObjectId(id))
-            return res.status(400).send("id isn't valid");
+            return res.status(400).send("id לא תקין");
         let user = await User.findByIdAndDelete(id);
         if (!user)
-            return res.status(404).send("no such user");
+            return res.status(404).send("לא קיים כזה משתמש");
         res.json(user);
     }
     catch (err) {
-        res.status(400).send("problem " + err.message);
+        res.status(400).send("הבעיה: " + err.message);
     }
 }
 
@@ -93,10 +93,10 @@ export const updateUserById = async (req, res) => {
     let { name } = req.body;
     try {
         if (!mongoose.isValidObjectId(id))
-            return res.status(400).send("id inst valid");
+            return res.status(400).send("id לא תקין");
         let user = await User.findById(id);
         if (!user)
-            return res.status(404).send("no such user");
+            return res.status(404).send("לא קיים כזה משתמש");
         user.name = name || user.name;
         user.password = numPages || user.password;
         user.email = audience || user.email;
@@ -105,6 +105,6 @@ export const updateUserById = async (req, res) => {
         res.json(user);
     }
     catch (err) {
-        res.status(400).send("its impossible to update this user" + err.message);
+        res.status(400).send("לא אפשרי לערוך את המשתמש: " + err.message);
     }
 }
